63a30d3a103f08306e7e8c91890eda0337b593d4
jallend1 <jallend1@gmail.com>
2020-05-15 13:05:53 -0700

    Adds fish component, loads sample fish, renders fish to the menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,37 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     };
+    
     addFish = fish => {
         const fishes = {...this.state.fishes};
         fishes[`fish${Date.now()}`] = fish;
         this.setState({
             fishes
-        })
+        });
     }
+    loadSampleFish = () => {
+        this.setState({fishes: sampleFishes})
+    }
+
     render(){
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                     <Order />
-                    <Inventory addFish={this.addFish} />
+                    <Inventory addFish={this.addFish} loadSampleFish={this.loadSampleFish} />
             </div>
         )
     }
